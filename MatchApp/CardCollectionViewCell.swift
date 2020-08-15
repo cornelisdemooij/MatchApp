db17f3fb58e4c0e30173f575a7042f99e525b4f9
Cornelis de Mooij <cornelis.de.mooij@ing.com>
2020-08-15 21:08:10 +0200

    Cards now flip up and down with animation on click

--- MatchApp/CardCollectionViewCell.swift
+++ MatchApp/CardCollectionViewCell.swift
@@ -14,4 +14,44 @@ class CardCollectionViewCell: UICollectionViewCell {
     
     @IBOutlet weak var backImageView: UIImageView!
     
+    var card:Card?
+    
+    func configureCell(card:Card) {
+        
+        // Keep trakc of the card that this cell represents
+        self.card = card
+        
+        // Set the front image view to the image that represents the card
+        frontImageView.image = UIImage(named: card.imageName)
+        
+        // Reset the state of the cell by checking the flipped status of the card and then showing the front or the back imageview accordingly
+        if card.isFlipped == true {
+            // Show the front image view
+            flipUp(timeInterval: TimeInterval(0))
+        } else {
+            // Show the back image view
+            flipDown(timeInterval: TimeInterval(0))
+        }
+    }
+    
+    func flipUp(timeInterval:TimeInterval = 0.3) {
+        UIView.transition(from: backImageView, to: frontImageView, duration: timeInterval, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        card?.isFlipped = true
+    }
+    
+    func flipDown(timeInterval:TimeInterval = 0.3) {
+        UIView.transition(from: frontImageView, to: backImageView, duration: timeInterval, options: [.transitionFlipFromLeft, .showHideTransitionViews], completion: nil)
+        card?.isFlipped = false
+    }
+    
+    func flipToggle(timeInterval:TimeInterval = 0.3) {
+        if let actualCard = card {
+            if actualCard.isFlipped {
+                flipDown(timeInterval: timeInterval)
+            } else {
+                flipUp(timeInterval: timeInterval)
+            }
+        }
+    }
+    
 }
--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -35,13 +35,25 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         // Get a cell
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
         
-        // TODO: Configure it
-        //let imageName = cardsArray[indexPath.row].imageName
+        // get the card from the card array
+        let card = cardsArray[indexPath.row]
+        
+        // Configure it
+        cell.configureCell(card: card)
 
         // Return it
         return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        // Get a reference to the cell that was tapped
+        let cell = collectionView.cellForItem(at: indexPath) as? CardCollectionViewCell
+        
+        cell?.flipToggle()
+        
     }
 
 }
